87e422bf2f7d3fe1057ab8319e6bbf9009ff544d
David73-Coloa <xdsomu851@gmail.com>
2023-01-09 19:04:08 -0300

    Second commit

--- lua/orchidian/init.lua
+++ lua/orchidian/init.lua
@@ -1,10 +1,17 @@
-local colors = require "orchidian.utils".getColors()
+--local colors = require("orchidian.utils").getColors()
+local groups = require('orchidian.groups') 
+local function setGroup() 
+end
+
 local function setHighlightGroups() 
-  vim.api.nvim_set_hl(0, "Normal",  { fg = colors.fg, bg = colors.bg, })
-  vim.api.nvim_set_hl(0,"Comment", { fg = colors.comment, })
-  vim.api.nvim_set_hl(0,"Statement", { fg = colors.red })
-  vim.api.nvim_set_hl(0,"Function", { fg = colors.purple })
-  vim.api.nvim_set_hl(0,"Identifier", { fg = colors.purple })
+  for group, color in pairs(groups) do 
+      vim.api.nvim_set_hl(0, group, color)
+   end
+  --vim.api.nvim_set_hl(0, "Normal",  { fg = colors.fg, bg = colors.bg, })
+  --vim.api.nvim_set_hl(0,"Comment", { fg = colors.comment, })
+  --vim.api.nvim_set_hl(0,"Statement", { fg = colors.red })
+  --vim.api.nvim_set_hl(0,"Function", { fg = colors.purple })
+  --vim.api.nvim_set_hl(0,"Identifier", { fg = colors.purple })
 
 
 
@@ -13,7 +20,7 @@ end
 
 
 local function load()
-  vim.o.background = "dark"
+   vim.o.background = "dark"
   vim.o.termguicolors = true 
   vim.g.colors_name = "orchidian"
   setHighlightGroups()
--- lua/orchidian/utils.lua
+++ lua/orchidian/utils.lua
@@ -1,14 +1,5 @@
-local function getColors()
-  return {
-    bg = "#0e0d0d",
-    fg = "#FAFAFA",
-    purple = "#BD93F9",
-    comment = "#565575",
-    red = "#ff5458"
-
-  }
-end
 
 return {
-   getColors = getColors
+   --getColors = getColors,
+   --getGroups = getGroups
 }
